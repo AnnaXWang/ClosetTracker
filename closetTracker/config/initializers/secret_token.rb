f09e3dc665b288bbb8d5eadb7aaee0387d49cf33
Anna <annaxw@stanford.edu>
2014-07-25 12:18:44 -0500

    Rails Foundation

---
+++ closetTracker/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClosetTracker::Application.config.secret_key_base = 'aab531ca6a149a36ccb484906c323b80f5fa758b79d3e7401a4eff41439bfcec7a113ac3a8a2bb1f17b1b3b157fbed28d86b4819f056ca601796790fe3d2cc48'
---
+++ closetTracker/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ClosetTracker::Application.config.session_store :cookie_store, key: '_closetTracker_session'
