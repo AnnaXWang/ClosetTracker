385e1b32f9dc51c314eb46c81d8945b7012050ea
Anna <annaxw@stanford.edu>
2014-07-25 13:54:20 -0500

    Creates Outfit Views

---
+++ closetTracker/app/controllers/outfits_controller.rb
@@ -0,0 +1,22 @@
+class OutfitsController < ApplicationController
+  def new
+    @title = "Upload A New Outfit"
+	@outfit = Outfit.new
+  end
+  def create
+    if params[:outfit]
+      @new_outfit = Outfit.new(:date_time => DateTime.now, :file_name => params[:outfit][:uploaded_outfit].original_filename)
+      if @new_outfit.save
+        @new_outfit.create_outfit(params[:outfit][:uploaded_outfit]) 
+      else
+     	render :controller => :outfits, :action => :new
+      end	
+    else
+      render :controller => :outfits, :action => :new
+  	end
+  end
+  def show
+  	@all_outfits = Outfit.all
+  	@title = "All Your Outfits"
+  end
+end
--- closetTracker/config/routes.rb
+++ closetTracker/config/routes.rb
@@ -1,4 +1,8 @@
 ClosetTracker::Application.routes.draw do
+  resources :contacts do
+    resources :outfits do
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
